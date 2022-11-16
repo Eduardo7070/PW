494deb6ebf12bca6a5ec15c183c438a7cfcfd1ae
Eduardo7070 <112709921+Eduardo7070@users.noreply.github.com>
2022-11-16 16:38:29 -0300

    Add files via upload

---
+++ meses.js
@@ -0,0 +1,40 @@
+var meses = (prompt("digite o mes do seu aniversario"))-1
+  ; 
+switch (meses){
+case 0 :
+alert ("Janeiro")    
+break;
+case 1 : 
+alert ("fevereiro ")    
+break;
+case 2 :
+alert ("março") 
+break;
+case 3 :
+alert ("abril")   
+break;
+case 4 : 
+alert ("maio")    
+break;
+case 5 : 
+alert ("junho")    
+break;
+case 6 :
+alert ("julho")    
+break;
+case 7 :
+alert ("agosto")    
+break;
+case 8 : 
+alert ("setembro ")    
+break ;
+case 9 : 
+alert ("outubro ")    
+break;  
+case 10 :
+alert("novembro")    
+break ;
+case 11 :
+alert("dezembro")    
+break ;
+}
---
+++ scripit.js
@@ -0,0 +1,7 @@
+var númeroaq                                                                                    QQ                                                                                                          QQ99 = window.prompt ("Digite um número:");
+var i, y;
+
+for (i=0;i<=10;i++){
+  y = número*i;
+   document.write(+i,"x",+número, "=", +y, "<br>" ) ;
+}
